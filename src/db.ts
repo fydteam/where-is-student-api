7f869fe7e016944b2254c86747a0d8b2a756d8eb
myrlagksruf <myrlagksruf@gmail.com>
2023-12-21 19:21:22 +0900

    db port 추가

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,7 @@ import { Person } from "./model/user.model";
 
 export const sequelize = new Sequelize(Bun.env.HAN_POSTGRES_DB ?? 'db', Bun.env.HAN_POSTGRES_USER ?? 'user', Bun.env.HAN_POSTGRES_PASSWORD ?? 'password', {
     host: Bun.env.HAN_POSTGRES_HOST ?? 'localhost',
+    port: Number(Bun.env.HAN_POSTGRES_PORT ?? '5432'),
     dialect: 'postgres',
     models:[ Person ]
 });
